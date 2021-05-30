cfff9a36f89b1f6b87aab289b93744bc0a0ddb6c
LaubetBeltran <laubetpop@gmail.com>
2021-05-29 20:52:17 -0500

    mock viewpara enlace de modales con botones

--- src/Views/MockView.js
+++ src/Views/MockView.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import ModalPreSingup from '../Components/ModalPreSignup';
+import { CustomDialog} from 'react-st-modal';
+import ModalAgenda from "../Components/ModalAgenda";
+import ModalInfoPostulant from "../Components/ModalInfoPostulant";
 
 const MockView = () => {
+	const openModal= async (ModalComponent) => {
+		const result = await CustomDialog(
+			<ModalComponent />,
+			{
+				title: ' ',
+				showCloseIcon: true,
+			}
+		);
+	}
   return (
+		<>
     <div>Ilse y Lau</div>
+		<button onClick={()=>openModal(ModalPreSingup)}>Modal Pre Registro</button>
+		<button onClick={()=>openModal(ModalAgenda)}>Modal Agendar Entrevista</button>
+		<button onClick={()=>openModal(ModalInfoPostulant)}>Modal Info Candidato</button>
+		</>
   )
 }
 
