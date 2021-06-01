e4d96b4d7c2141f98b2d0765c35f0588577007ee
LaubetBeltran <laubetpop@gmail.com>
2021-05-31 21:34:10 -0500

    match preregistro con registro

--- src/Components/SignUpForm.jsx
+++ src/Components/SignUpForm.jsx
@@ -10,197 +10,260 @@ import openModal from '../Components/ModalFunction';
 import ModalPrivacy from '../Components/ModalPrivacy';
 
 const initialInputs = {
-    email:'',
-    password:''
+	email: '',
+	password: ''
 }
 
 export const SignUpForm = () => {
 	const auth = firebase.auth();
- 	const db = firebase.firestore();
-	const history = useHistory(); 
+	const db = firebase.firestore();
+	const history = useHistory();
 
-  const [name, setName] = useState("");
-  const [rfc, setRfc] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState(initialInputs);
-  const [password, setPassword] = useState(initialInputs);
-	const [recruiter, setRecruiter] = useState('');
+	const [name, setName] = useState("");
+	const [rfc, setRfc] = useState("");
+	const [phone, setPhone] = useState("");
+	const [email, setEmail] = useState(initialInputs);
+	const [password, setPassword] = useState(initialInputs);
+	// const [recruiterName, setRecruiterName] = useState('');
 	const [idPreSign, setIdPreSign] = useState('');
 	const status = 'initial';
 	const cv = 'none';
 	const interviews = [];
+	const[samePassword, setSamePasword] = useState();
+
+	const info = async () => {
+		db.collection('preSignUp').doc(email).get()
+			.then((res) => {
+				console.log(res.data().rfc, res)
+				return res.data()
+			})
+			.then((res) => {
+				console.log(res);
+				auth.createUserWithEmailAndPassword(email, password)
+					.then(() => {
+						console.log('registrado')
+						db.collection("candidates").doc().set({ name, rfc, phone, email, 'recruiter': res.recruiter, status, cv, interviews, 'vacant': res.vacant })
+						db.collection("users").doc(email).set({ name, email, 'permission': 'applicant' });
+						db.collection('preSignUp').doc(email).delete();
+						history.pushState('./');
+					})
+					.catch(() => {
+						console.log('no registrado :c')
+					})
+			})
+			.catch((err) => {
+				// addToast(
+				// 	"Necesitas estar dado de alta en la plaaforma para poder registrarte, contacta a tu recluador",
+				// 	{
+				// 		autoDismiss: true,
+				// 		placement: "top-right",
+				// 		appearance: "warning",
+				// 	}
+				// );
+				console.log(err, 'no se encontró usuario')
+			})
+	}
+
+
+	const [isVerified, setIsVerified] = useState(false);
+	const { addToast } = useToasts();
+
+	// const signUpBtn = () => {
+	//   if (isVerified && email && password) {
+	//     firebase
+	//       .auth()
+	//       .signInWithEmailAndPassword(email, password)
+	//       .then(() => {
+	//         history.push("/");
+	//       })                                                                                          
+	//       .catch(function (error) {
+	//         console.log(error);
+	//         addToast("Correo o contraseña incorrectos", {
+	//           autoDismiss: true,
+	//           placement: "top-right",
+	//           appearance: "error",
+	//         });
+	//       });
+	//   } else {
+	//     addToast(
+	//       "Ingresa tu correo y contraseña, y muéstranos que no eres un robot",
+	//       {
+	//         autoDismiss: true,
+	//         placement: "top-right",
+	//         appearance: "warning",
+	//       }
+	//     );
+	//   }
+	// };
 	
-  
-const info = async (e) =>{
- e.preventDefault();
- db.collection('preSignUp').doc(rfc).get()
-	.then((res)=>{
-		console.log(res.id, 'soy el id')
-		setRecruiter(res.data().recruiter);
-		setIdPreSign(res.id);
-		auth.createUserWithEmailAndPassword(email, password)
-    .then(()=>{
-        console.log('registrado')
-        history.pushState('./') 
-				db.collection("candidates").doc().set({name, rfc, phone, email, recruiter, status, cv, interviews});
- 				db.collection("users").doc(email).set({name, email, 'premission' :'applicant'});
- 				db.collection('preSignUp').doc(rfc).delete();
-    })
-    .catch(()=>{
-        console.log('no registrado :c')
-    })
-	})
+	const pasworInput = ()=> {
+		const pwdText= document.getElementById('loginPsw').value
+		const confirmpwdText = document.getElementById('loginPswConfirmed').value
+		if (confirmpwdText !== '' && pwdText !== 'confirmpwdText') {
+			console.log('confirme contraseña');
+			setSamePasword('empty');
+		} else if (confirmpwdText === '' && pwdText ===''){
+			samePassword('conraseñas vacías');
+			setSamePasword('empty')
+		}else if (confirmpwdText === pwdText){
+			console.log('contraseñas iguales');
+			setSamePasword(true)
+		} else if (confirmpwdText !== pwdText){
+			console.log('contraseñas diferentes');
+			setSamePasword(false)
+		}
+	}
+	const confirmPasworInput = ()=> {
+		const pwdText= document.getElementById('loginPsw').value
+		const confirmpwdText = document.getElementById('loginPswConfirmed').value
+		if (pwdText !== '' && pwdText !== confirmpwdText) {
+			console.log('contraseñas diferentes');
+			setSamePasword(false);
+		} else if (confirmpwdText === '' && pwdText ===''){
+			samePassword('conraseñas vavías');
+			setSamePasword('empty')
+		} else if (confirmpwdText === pwdText){
+			console.log('contraseñas iguales');
+			setSamePasword(true)
+		}else if (confirmpwdText !== pwdText){
+			console.log('contraseñas diferentes');
+			setSamePasword(false)
+		}
+	}
 
-}
 
-const [isVerified, setIsVerified] = useState(false);
-const { addToast } = useToasts();
 
-// const signUpBtn = () => {
-//   if (isVerified && email && password) {
-//     firebase
-//       .auth()
-//       .signInWithEmailAndPassword(email, password)
-//       .then(() => {
-//         history.push("/");
-//       })                                                                                          
-//       .catch(function (error) {
-//         console.log(error);
-//         addToast("Correo o contraseña incorrectos", {
-//           autoDismiss: true,
-//           placement: "top-right",
-//           appearance: "error",
-//         });
-//       });
-//   } else {
-//     addToast(
-//       "Ingresa tu correo y contraseña, y muéstranos que no eres un robot",
-//       {
-//         autoDismiss: true,
-//         placement: "top-right",
-//         appearance: "warning",
-//       }
-//     );
-//   }
-// };
- 
 
-const privacyModal = () => {
-  openModal(ModalPrivacy);
-}
 
-return (
-  <div className="signUp-container">
-    <img className="signUpbackImg" src={loginBack} alt="signUpbackImg" />
 
-    <form className="signUp-form-container">
-      <p>Registro de Postulantes</p>
+	const privacyModal = () => {
+		openModal(ModalPrivacy);
+	}
 
-      <div id="signUp-form-container-w">
-        <div className="formLR">
-          <div className="labelInputSgngUp">
-            <label className="namee" for="signUpName">
-              Nombre
+	return (
+		<div className="signUp-container">
+			<img className="signUpbackImg" src={loginBack} alt="signUpbackImg" />
+
+			<form className="signUp-form-container" onSubmit={(e) => {
+				e.preventDefault()
+				info()
+				console.log('hola')
+			}}>
+				<p>Registro de Postulantes</p>
+
+				<div id="signUp-form-container-w">
+					<div className="formLR">
+						<div className="labelInputSgngUp">
+							<label className="namee" for="signUpName">
+								Nombre
             </label>
-            <input
-              type="text"
-              id="signUpName"
-              name="nombre"
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className="labelInputSgngUp">
-            <label for="RFC">RFC</label>
-            <input
-              type="text"
-              id="signUpRFC"
-              rfc=""
-              onChange={(e) => setRfc(e.target.value)}
-            />
-          </div>
-          <div className="labelInputSgngUp">
-            <label for="Telefono">Telefono</label>
-            <input
-              type="number"
-              id="signUpnumber"
-              phone=""
-              onChange={(e) => setPhone(e.target.value)}
-            />
-          </div>
-        </div>
+							<input
+								type="text"
+								id="signUpName"
+								name="nombre"
+								onChange={(e) => setName(e.target.value)}
+							/>
+						</div>
+						<div className="labelInputSgngUp">
+							<label for="RFC">RFC</label>
+							<input
+								type="text"
+								id="signUpRFC"
+								rfc=""
+								onChange={(e) => setRfc(e.target.value)}
+							/>
+						</div>
+						<div className="labelInputSgngUp">
+							<label for="Telefono">Telefono</label>
+							<input
+								type="number"
+								id="signUpnumber"
+								phone=""
+								onChange={(e) => setPhone(e.target.value)}
+							/>
+						</div>
+					</div>
 
-        <div className="formLR">
-          <div className="labelInputSgngUp">
-            <label for="loginUser">Correo electrónico</label>
-            <input
-              type="email"
-              id="singUpUser"
-              email=""
-              value={email.email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="labelInputSgngUp">
-            <label for="loginPsw">Contraseña</label>
-            <input
-              type="password"
-              id="loginPsw"
-              password=""
-              value={password.password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="labelInputSgngUp">
-            <label for="loginPswConfirmed">Confirma tu Contraseña</label>
-            <input type="password" id="loginPswConfirmed" />
-          </div>
-          {/* if (loginPsw !=== loginPswConfirmed) {
+					<div className="formLR">
+						<div className="labelInputSgngUp">
+							<label for="loginUser">Correo electrónico</label>
+							<input
+								type="email"
+								id="singUpUser"
+								email=""
+								value={email.email}
+								onChange={(e) => setEmail(e.target.value)}
+							/>
+						</div>
+						<div className="labelInputSgngUp">
+							<label for="loginPsw">Contraseña</label>
+							<input
+								type="password"
+								id="loginPsw"
+								password=""
+								// value={password.password}
+								onChange={(e) => {
+									pasworInput()
+									setPassword(e.target.value)
+								}}
+							/>
+						</div>
+						<div className="labelInputSgngUp">
+							<label for="loginPswConfirmed">Confirma tu Contraseña</label>
+							<input type="password" id="loginPswConfirmed" 
+							password=""
+							// value={password.password}
+							onChange={(e) => {
+								confirmPasworInput()
+								setPassword(e.target.value)
+							}}/>
+						</div>
+						{/* if (loginPsw !=== loginPswConfirmed) {
                             <label> No coinciden las contraseñas</label>
                         }
                         */}
-        </div>
-      </div>
-      <div className="AvisoDePrivacidad">
-          <input
-            type="checkbox"
-            className="checkbox-politics"
-            id="politics"
-            value="true"
-            // onChange={politicsChecked}
-          />
-          <label for="politics" className="politics">
-            Acepto el Aviso Legal sobre Protección de Datos Personales y los{" "}
-            <a
-              target="_blanck"
-              // href="https://www.montepiedad.com.mx/legales-aviso-de-privacidad-empleo"
-              className="a-politics"
-              onClick={privacyModal}
-            >
-              <span>Términos y Condiciones </span>
-            </a>{" "}
+					</div>
+				</div>
+				<div className="AvisoDePrivacidad">
+					<input
+						type="checkbox"
+						className="checkbox-politics"
+						id="politics"
+						value="true"
+					// onChange={politicsChecked}
+					/>
+					<label for="politics" className="politics">
+						Acepto el Aviso Legal sobre Protección de Datos Personales y los{" "}
+						<a
+							target="_blanck"
+							// href="https://www.montepiedad.com.mx/legales-aviso-de-privacidad-empleo"
+							className="a-politics"
+							onClick={privacyModal}
+						>
+							<span>Términos y Condiciones </span>
+						</a>{" "}
             de este concurso.
           </label>
-        </div>
+				</div>
 
 
-      <ReCaptchaComponent
-        isVerified={isVerified}
-        setIsVerified={setIsVerified}
-      />
+				<ReCaptchaComponent
+					isVerified={isVerified}
+					setIsVerified={setIsVerified}
+				/>
 
-      <input
-        type="submit"
-        className={
-          isVerified && email && password ? "btn-enabled" : "btn-disabled"
-        }
-        id="loginBtn"
-        value="INGRESAR"
-        onClick={info}
-      />
-      <div className="AvisoDePrivacidad"></div>
-    </form>
-  </div>
-);
+				<input
+					type="submit"
+					className={
+						isVerified && email && password ? "btn-enabled" : "btn-disabled"
+					}
+					id="loginBtn"
+					value="INGRESAR"
+				/>
+				<div className="AvisoDePrivacidad"></div>
+			</form>
+		</div>
+	);
 };
 
 
